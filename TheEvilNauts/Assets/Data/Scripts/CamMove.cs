51b8f33fefff343401c540c2e67db0f19b1944c5
ASarana <andrey.sarana@gmail.com>
2016-10-22 13:21:53 +0300

    Перенос материала с тестовой площадки
    - Модель обновлена
    - Настроен Масштаб, теперь 1 метр = 10 юнитам
    - Наброски текстур для террейна
    - Перенес все скрипты, требуется настройка

---
+++ TheEvilNauts/Assets/Data/Scripts/CamMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMove : MonoBehaviour
+{
+
+    public Vector3 camzero;
+    public Vector3 camtrans;
+    public Transform hero;
+    void Start()
+    {
+        // camzero = new Vector3(-40, -44, 2);
+        //camzero = hero.position - this.transform.position; //начальное положение камера относительно пакмана
+        this.transform.rotation = Quaternion.Euler(camtrans);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = hero.position - camzero; //двигаем камеру в след за пакманом
+       // print(camzero);
+    }
+}
