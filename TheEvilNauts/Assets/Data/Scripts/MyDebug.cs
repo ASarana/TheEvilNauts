4b5b29e49643a26678eb75ad4d1e00e39c7ee0a2
ASarana <andrey.sarana@gmail.com>
2016-10-24 22:13:36 +0300

    Перенос с тестовой площадки 0.3.2 (реанимация)
    - editor убран
    - ошибки исключены

---
+++ TheEvilNauts/Assets/Data/Scripts/MyDebug.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MyDebug : MonoBehaviour {
+
+    // Use this for initialization
+    int debi = 20;
+    string[] names = new string[20];
+    string[] param = new string[20];
+    public TextMesh debtext;
+    void Start ()
+    {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddParamDebug (string _name, string _param)
+    {
+        if (_name == null) return;
+        for (int i = 0; i < debi; i++)
+        {
+            if (names[i] == _name)
+            {
+                param[i] = _param;
+                return;
+            }
+        }
+        for (int i = 0; i < debi; i++)
+        {
+            if (names[i] == null)
+            {
+                names[i] = _name;
+                param[i] = _param;
+                return;
+            }
+        }
+    }
+
+    public void ShowDebug()
+    {
+        debtext.text = null;
+        for (int i = 0; i < debi; i++)
+        {
+            if(param[i] != null) debtext.text += (names[i] + ": " + param[i] + '\n');
+        }
+    }
+}
