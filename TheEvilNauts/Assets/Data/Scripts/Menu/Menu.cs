16b00946419ce0c8fbb024a8412819096007db38
ASarana <andrey.sarana@gmail.com>
2017-01-12 20:28:41 +0300

    Обновление до 5.5
    - меню (неработает)
    - поправил настройки анимации
    - player отключен в инспекторе для меню

---
+++ TheEvilNauts/Assets/Data/Scripts/Menu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+	public GameObject StartMenu;
+	public GameObject Character;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void PressStart()
+	{
+		Debug.Log("<color=red>Start</color>"); 
+		StartMenu.SetActive(false);
+		Character.SetActive (true);
+	}
+
+	public void PressQuit()
+	{
+		Debug.Log("<color=red>Quit</color>"); 
+		//Application.Quit ();
+	}
+}
