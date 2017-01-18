43cda6cb06ce01bb873dbce44606c42c8d012bc7
SrnAnton <srn.anton@gmail.com>
2017-01-18 20:15:43 +0300

    fix menu

---
+++ TheEvilNauts/Assets/Data/Scripts/Gui/RollControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollControl : MonoBehaviour {
+
+	public Image targetImage;
+	public float rollSpeed = 1F;
+	float RollingTime = 0;
+	int angle = 90;
+	// Use this for initialization
+	void Start () 
+	{
+		if (targetImage == null)
+			targetImage = GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (RollingTime > 0) {
+			RollingStone ();
+			RollingTime -= (RollingTime*rollSpeed)/angle;
+		}
+
+	}
+
+	public void PlsRollMe()
+	{
+		RollingTime += angle;
+	}
+
+	void RollingStone () 
+	{
+		float speed =(RollingTime*rollSpeed)/angle;
+		Vector3 angleOfRotation = new Vector3 (0, 0,speed);
+
+		targetImage.rectTransform.Rotate (angleOfRotation);
+	}
+}
---
+++ TheEvilNauts/Assets/Data/Scripts/Menu/CogRoll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Image))]
+public class CogRoll : MonoBehaviour {
+
+	public Image targetImage;
+	public float rollSpeed = 1.5F;
+	// Use this for initialization
+	void Start () 
+	{
+		if (targetImage == null)
+			targetImage = GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		Vector3 angle = new Vector3 (0, 0,rollSpeed);
+	
+		targetImage.rectTransform.Rotate (angle);
+	}
+}
